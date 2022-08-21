e8e765b16cae8750913a348c146de84a8b27760a
ankitrjshivastav@gmail.com <ankitrjshrivastav@gmail.com>
2022-08-21 16:07:00 +0530

    first commit

---
+++ MernStack-Blog-App-Frontend/src/components/Header.js
@@ -0,0 +1,123 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  AppBar,
+  Typography,
+  Toolbar,
+  Box,
+  Button,
+  Tabs,
+  Tab,
+  Stack,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { authActions } from "../store";
+import { useStyles } from "./utils";
+const Header = () => {
+  const classes = useStyles();
+  const dispath = useDispatch();
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+
+  const [value, setValue] = useState();
+
+  const [user, setUser] = useState("");
+  const id = localStorage.getItem("userId");
+  const sendRequest = async () => {
+    const res = await axios
+      .get(`https://apnablogs.herokuapp.com/api/blog/user/${id}`)
+      .catch((err) => console.log(err));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setUser(data.user));
+  }, [isLoggedIn]);
+  console.log(user.name);
+
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        background:
+        "linear-gradient(90deg, #3a5311 2%, #728c69 36%, #aef359 73%, #234f1e 100%)",
+      }}
+    >
+      <Toolbar>
+        <Typography className={classes.font} variant="h4">
+          BlogsApp
+        </Typography>
+        {isLoggedIn && (
+          <Box display="flex" marginLeft={"auto"} marginRight="auto">
+            <Tabs
+              textColor="inherit"
+              value={value}
+              onChange={(e, val) => setValue(val)}
+            >
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs"
+                label="All Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/myBlogs"
+                label="My Blogs"
+              />
+              <Tab
+                className={classes.font}
+                LinkComponent={Link}
+                to="/blogs/add"
+                label="Add Blog"
+              />
+            </Tabs>
+          </Box>
+        )}
+        <Box display="flex" marginLeft="auto">
+          {!isLoggedIn && (
+            <>
+              {" "}
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Login
+              </Button>
+              <Button
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Signup
+              </Button>
+            </>
+          )}
+          {isLoggedIn && (
+            <Stack direction="row" spacing={2}>
+              <Typography variant="h6">{user.name}</Typography>
+              <Button
+                onClick={() => dispath(authActions.logout())}
+                LinkComponent={Link}
+                to="/auth"
+                variant="contained"
+                sx={{ margin: 1, borderRadius: 10 }}
+                color="warning"
+              >
+                Logout
+              </Button>
+            </Stack>
+          )}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
